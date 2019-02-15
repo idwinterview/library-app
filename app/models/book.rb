fab238eee112e90d2a2e7daa3835db908fa75f68
plato721 <matt.ewell@gmail.com>
2019-02-14 17:03:06 -0700

    Add check_in helper methods to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,16 @@
 class Book < ActiveRecord::Base
   has_one :customer_book, as: :bookable
   has_one :customer, through: :customer_book
+
+  def status
+    customer_book&.status
+  end
+
+  def checked_out?
+    status == 'checked out'
+  end
+
+  def check_in
+    customer_book.update(status: 'returned')
+  end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -9,4 +9,47 @@ shared_examples :book do |factory_hash|
 
     expect(object.customer).to eql(customer)
   end
+
+  context '#status' do
+    it "delegates to its customer_book" do
+      customer = create(:customer)
+      book = create(:book)
+
+      customer.books << book
+
+      expect(book.status).to eql(book.customer_book.status)
+    end
+  end
+
+  context '#checked_out?' do
+    it 'is checked out if status is checked out' do
+      book = create(:book)
+
+      allow(book).to receive(:status).and_return('checked out')
+
+      expect(book.checked_out?).to be_truthy
+    end
+
+    it 'is not checked out if it has no customer_book' do
+      book = create(:book)
+
+      expect(book.customer_book).to be_nil
+      expect(book.checked_out?).to be_falsey
+    end
+  end
+
+  context '#check_in' do
+    it 'can be checked in' do
+      book = create(:book)
+      customer = create(:customer)
+      customer.books << book
+
+      expect(book.checked_out?).to be_truthy
+
+      book.check_in
+
+      expect(book.checked_out?).to be_falsey
+      expect(book.customer_book.status).to eql('returned')
+    end
+  end
 end
