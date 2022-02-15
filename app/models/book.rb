43d8417347bb14538fc0cbfe09eb25971330a5dc
KingGranger <cheeridon@gmail.com>
2022-02-14 23:18:22 -0500

    coding assignment

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,28 @@
 class Book < ActiveRecord::Base
   belongs_to :customer
 
+  def initialize
+    @state = "In Stock"
+  end
+
+  def check
+    case @state
+    when "In Stock"
+      @state = "Checked Out"
+    when "Checked Out"
+      @state = "In Stock"
+    else
+      "Invalid Action"
+    end
+  end
+
+  def lost
+    case @state
+    when "In Stock" || "Checked Out"
+      @state = "Lost"
+    else
+      "Invalid Action"
+    end
+  end
+
 end
