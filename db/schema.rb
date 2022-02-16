e836fd184c0511248f98fa1a5a9372a1f0d1c819
Travis <travis.mckinstry@thoughtworks.com>
2022-02-16 10:42:49 -0700

    Adds Customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_144616) do
+ActiveRecord::Schema.define(version: 2022_02_16_174033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2018_05_23_144616) do
     t.integer "customer_id"
     t.integer "book_id"
     t.string "status", limit: 15, default: "checked out"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
