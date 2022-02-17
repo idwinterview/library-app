6fc53a1db8711ee8bd13785537986d57599b8b7d
Travis <travis.mckinstry@thoughtworks.com>
2022-02-17 06:10:01 -0700

    Adds test for 'books/list' as a librarian

--- spec/integration/book_spec.rb
+++ spec/integration/book_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'BookList' do
-  it 'should navigate to book show page' do
-    customer = Customer.create!(first_name: "George", last_name: "Hammel")
-    book = Book.create!(type: "", isbn: 2222, title: "The Book", author: "The Author", category: "Non-fiction")
-    CustomerBook.create!(user_id: customer.id, book_id: book.id)
+  describe 'as a customer' do
+    it "clicking on a books' title should navigate to book show page" do
+      customer = Customer.create!(first_name: "George", last_name: "Hammel")
+      book = Book.create!(type: "", isbn: 2222, title: "The Book", author: "The Author", category: "Non-fiction")
+      CustomerBook.create!(user_id: customer.id, book_id: book.id)
 
-    visit('/books/list')
+      visit('/books/list')
 
-    click_on("#{book.title}")
-    expect(current_path).to eq("/books/#{book.id}")
+      click_on("#{book.title}")
+      expect(current_path).to eq("/books/#{book.id}")
+    end
+  end
+
+  describe 'as a librarian' do
+    it 'should display all books' do
+      
+    end
   end
 end
